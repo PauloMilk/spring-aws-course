5837f45f3105d47424a06c4f831fe38c23d89f39
pauloMilk <pauloleitecosta14@gmail.com>
2020-05-29 20:31:06 -0300

    Adicionando os services

---
+++ src/main/java/com/paulo/springcourseaws/service/RequestStageService.java
@@ -0,0 +1,48 @@
+package com.paulo.springcourseaws.service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.paulo.springcourseaws.domain.RequestStage;
+import com.paulo.springcourseaws.domain.enums.RequestState;
+import com.paulo.springcourseaws.repository.RequestRepository;
+import com.paulo.springcourseaws.repository.RequestStageRepository;
+
+@Service
+public class RequestStageService {
+
+	@Autowired
+	private RequestStageRepository requestStageRepository;
+	
+	@Autowired
+	private RequestRepository requestRepository;
+	
+	public RequestStage save(RequestStage requestStage) {
+		requestStage.setRealizationDate(new Date());		
+		RequestStage created = requestStageRepository.save(requestStage);
+		requestRepository.updateStatus(requestStage.getRequest().getId(), requestStage.getState());
+		
+		return created;
+	}
+	
+	public RequestStage update(RequestStage requestStage) {
+		RequestStage updated = requestStageRepository.save(requestStage);
+		return updated;
+	}
+	
+	public List<RequestStage> listAllByRequestId(Long id) {
+		List<RequestStage> requestsStages = requestStageRepository.findAllByRequestId(id);
+		return requestsStages;
+	}
+	
+	public RequestStage getById(Long id) {
+		Optional<RequestStage> result = requestStageRepository.findById(id);
+		return result.get();
+	}
+	
+	
+}
---
+++ src/main/java/com/paulo/springcourseaws/service/UserService.java
@@ -0,0 +1,51 @@
+package com.paulo.springcourseaws.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.paulo.springcourseaws.domain.User;
+import com.paulo.springcourseaws.repository.UserRepository;
+import com.paulo.springcourseaws.service.util.HashUtil;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public User save(User user) {
+		String hash = HashUtil.getSecureHash(user.getPassword());
+		user.setPassword(hash);
+		
+		User createdUser = userRepository.save(user);
+		return createdUser;
+	}
+	
+	public User update(User user) {
+		String hash = HashUtil.getSecureHash(user.getPassword());
+		user.setPassword(hash);
+		
+		User updatedUser = userRepository.save(user);
+		return updatedUser;
+	}
+	
+	public User findById(Long id) {
+		Optional<User> result = userRepository.findById(id);
+		return result.get();
+	}
+	
+	public List<User> findAll() {
+		List<User> users = userRepository.findAll();
+		return users;
+	}
+	
+	public User login(String email, String password) {
+		String hash = HashUtil.getSecureHash(password);
+		Optional<User> result = userRepository.findByEmailAndPassword(email, hash);
+		return result.get();
+	}
+
+}
