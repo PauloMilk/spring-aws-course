7e8181bb3d7542dfb6acd73a215f769b4e9a8b84
pauloMilk <pauloleitecosta14@gmail.com>
2020-05-29 18:00:19 -0300

    Adicionando o mapeamento dos dominios

--- src/main/java/com/paulo/springcourseaws/domain/User.java
+++ src/main/java/com/paulo/springcourseaws/domain/User.java
@@ -3,6 +3,17 @@ package com.paulo.springcourseaws.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.paulo.springcourseaws.domain.enums.Role;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,12 +22,29 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter @Setter
+@Entity(name = "user")
 public class User {
 
+	@Id 
+	@GeneratedValue( strategy = GenerationType.IDENTITY )
 	private Long id;
+	
+	@Column(length = 75, nullable = false)
 	private String name;
+	
+	@Column(length = 75, nullable = false, unique = true)
 	private String email;
+	
+	@Column(length = 100, nullable = false)
 	private String password;
-	private List<Request> requests = new ArrayList<Request>();
-	private List<RequestStage> stages = new ArrayList<RequestStage>();
+	
+	@Column(length = 20, nullable = false)
+	@Enumerated(EnumType.STRING)
+	private Role role;
+	
+	@OneToMany(mappedBy = "user")
+	private List<Request> requests = new ArrayList<>();
+	
+	@OneToMany(mappedBy = "user")
+	private List<RequestStage> stages = new ArrayList<>();
 }
