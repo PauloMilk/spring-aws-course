4189e33f78f8ab75cb8717d065eeba2f969142fb
pauloMilk <pauloleitecosta14@gmail.com>
2020-05-29 15:14:04 -0300

    Construção do projeto base

---
+++ src/main/java/com/paulo/springcourseaws/SpringCourseAwsApplication.java
@@ -0,0 +1,13 @@
+package com.paulo.springcourseaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCourseAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCourseAwsApplication.class, args);
+	}
+
+}
